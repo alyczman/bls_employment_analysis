7b4b15a0baf9553d1b5e39e9a9f61f9ffbe2e0a1
Andrew Lyczmanenko <alyczman@gmail.com>
2025-08-20 22:22:29 -0400

    Staging data ahead of visualizations

--- visualizations/visualizations.py
+++ visualizations/visualizations.py
@@ -6,3 +6,8 @@ file_path = '../data/LaborForceParticipationRate.csv'
 df = pd.read_csv(file_path)
 print(df.head())
 
+month_col = df['period'].str[1:]
+df.insert(3, 'Month', month_col)
+
+df['YearMonth'] = df['year'].astype('str') + df['Month']
+print(df.head())
